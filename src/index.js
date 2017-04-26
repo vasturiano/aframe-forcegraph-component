16d032dff2c3e9a0983cf9cb985c4e5faf715809
vasco <vastur@gmail.com>
2017-04-26 03:05:33 -0700

    Refactor update

--- src/index.js
+++ src/index.js
@@ -26,25 +26,25 @@ AFRAME.registerComponent('forcegraph', {
     },
 
     init() {
-        this.forceLayout = d3.forceSimulation()
+        this.data.forceLayout = d3.forceSimulation()
             .numDimensions(3)
             .force('link', d3.forceLink().id(d => d.id))
             .force('charge', d3.forceManyBody())
             .force('center', d3.forceCenter())
             .stop();
 
-        this.nodes = [];
-        this.links = [];
+        this.data.nodes = [];
+        this.data.links = [];
     },
 
     update(oldData) {
-        const that = this,
+        const comp = this,
             elData = this.data,
-            diff = AFRAME.utils.diff(this.data, oldData);
+            diff = AFRAME.utils.diff(elData, oldData);
 
         if ('jsonUrl' in diff) {
             // (Re-)load data
-            d3.json(this.data.jsonUrl, json => {
+            d3.json(elData.jsonUrl, json => {
                 // Color brewer paired set
                 const colors = ['#a6cee3','#1f78b4','#b2df8a','#33a02c','#fb9a99','#e31a1c','#fdbf6f','#ff7f00','#cab2d6','#6a3d9a','#ffff99','#b15928'];
 
@@ -58,17 +58,17 @@ AFRAME.registerComponent('forcegraph', {
                     link.id = [link.source,link.target].join(' > ');
                 });
 
-                that.nodes = json.nodes;
-                that.links = json.links;
+                elData.nodes = json.nodes;
+                elData.links = json.links;
 
-                that.update(elData);  // Force re-update
+                comp.update(elData);  // Force re-update
             });
         }
 
         // Add children entities
         const d3El = d3.select(this.el);
         let nodes = d3El.selectAll('a-sphere.node')
-            .data(this.nodes, d => d.id);
+            .data(elData.nodes, d => d.id);
 
         nodes.exit().remove();
 
@@ -78,21 +78,21 @@ AFRAME.registerComponent('forcegraph', {
                 .classed('node', true)
                 .attr('segments-width', 8)	// Lower geometry resolution to improve perf
                 .attr('segments-height', 8)
-                .attr('radius', d => Math.cbrt(d[this.data.valField] || 1) * this.data.nodeRelSize)
+                .attr('radius', d => Math.cbrt(d[elData.valField] || 1) * elData.nodeRelSize)
                 .attr('color', d => '#' + (d.color || 0xffffaa).toString(16))
                 .attr('opacity', 0.75)
                 .on('mouseenter', d => {
                     console.log('in', d);
-                    //this.data.tooltipElem.attr('value', this.data.nameAccessor(d) || '');
+                    //elData.tooltipElem.attr('value', elData.nameAccessor(d) || '');
                 })
                 .on('mouseleave', () => {
                     console.log('out', d);
-                    //this.data.tooltipElem.attr('value', '');
+                    //elData.tooltipElem.attr('value', '');
                 })
         );
 
         let links = d3El.selectAll('a-entity.link')
-            .data(this.links, d => d.id);
+            .data(elData.links, d => d.id);
 
         links.exit().remove();
 
@@ -100,27 +100,27 @@ AFRAME.registerComponent('forcegraph', {
             links.enter()
                 .append('a-entity')
                 .classed('link', true)
-                .attr('line', `color: #f0f0f0; opacity: ${this.data.lineOpacity}`)
+                .attr('line', `color: #f0f0f0; opacity: ${elData.lineOpacity}`)
         );
 
         // Feed data to force-directed layout
-        this.forceLayout
+        elData.forceLayout
             .stop()
             .alpha(1)// re-heat the simulation
-            .nodes(this.nodes)
-            .force('link').links(this.links);
+            .nodes(elData.nodes)
+            .force('link').links(elData.links);
 
-        for (let i=0; i<this.data.warmUpTicks; i++) { this.forceLayout.tick(); } // Initial ticks before starting to render
+        for (let i=0; i<elData.warmUpTicks; i++) { elData.forceLayout.tick(); } // Initial ticks before starting to render
 
         let cntTicks = 0;
         const startTickTime = new Date();
-        this.forceLayout.on("tick", layoutTick).restart();
+        elData.forceLayout.on("tick", layoutTick).restart();
 
         //
 
         function layoutTick() {
-            if (cntTicks++ > that.data.coolDownTicks || (new Date()) - startTickTime > that.data.coolDownTime) {
-                that.forceLayout.stop(); // Stop ticking graph
+            if (cntTicks++ > elData.coolDownTicks || (new Date()) - startTickTime > elData.coolDownTime) {
+                elData.forceLayout.stop(); // Stop ticking graph
             }
 
             // Update nodes position
